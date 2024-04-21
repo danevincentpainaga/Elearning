8852cbe1c2d0d3ca4f6ce0d29ba3d4fc9fe8dc7c
danepainaga <danepainaga@gmail.com>
2024-04-21 23:09:58 +0800

    Updated manage-teachers add-subject

--- src/app/manage-teachers/add-subjects/add-subjects.component.ts
+++ src/app/manage-teachers/add-subjects/add-subjects.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { DropdownChangeEvent } from 'primeng/dropdown/dropdown.interface';
 import { AdminService } from '../../services/admin.service';
 
@@ -11,12 +11,14 @@ import { AdminService } from '../../services/admin.service';
 export class AddSubjectsComponent {
 
   showTable!: boolean;
-  period = [{ period_id: 1, period: '1st Grading' }];
-  academicYear = [{ academic_year: '2023-2024', ayID: 1 }];
+  period = [{ periodID: 1, period: '1st Grading' }];
+  academicYear = [{ academicYear: '2023-2024', academicYearID: 1 }];
   selectedSem!: any;
   teachers!: any[];
   subjects!: any[];
-  selectedTeacher: any;
+  selectedTeacher!:string;
+  selectedPeriod!: string;
+  selectedAcademicYear!: string;
   tSubjecForm: FormGroup = this.fb.group({
     newSubjects: this.fb.array([]),
   });
@@ -28,8 +30,8 @@ export class AddSubjectsComponent {
 
   ngOnInit() {
     this.teachers = [
-      { name: 'Juan', teacher_id: 1 },
-      { name: 'Pedro', teacher_id: 2 },
+      { name: 'Juan', teacherID: 1 },
+      { name: 'Pedro', teacherID: 2 },
     ];
 
     this.adminSrvc.getSubjects().then(res => {
@@ -45,13 +47,17 @@ export class AddSubjectsComponent {
   addNewSubject(): void {
     this.newSubjects.push(
       this.fb.group({
-        subject: [null, Validators.required],
-        description: [null, Validators.required],
-        period: [null, Validators.required],
-        academic_year: [null, Validators.required],
-        teacher_id: [this.selectedTeacher.teacher_id],
+        subject: [null, [Validators.required, this.uniqueSubject(this.newSubjects)]],
+        description: [null, Validators.required]
       })
     );
+  }
+
+  uniqueSubject(newSubject: any): ValidatorFn  {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const res = newSubject.value.findIndex((val: any) => (val.subject && val.subject === control.value));
+      return res > -1 ? { Duplicate: true } : null;
+    }
   }
      
   removeRow(index: any): void {
@@ -62,9 +68,10 @@ export class AddSubjectsComponent {
     let subjects = this.newSubjects.value.map((val: any) => {
       return {
         ...val,
-        academic_year: val.academic_year.ayID,
-        period: val.period.period_id,
-        subject: val.subject.subject_id
+        subject: val.subject,
+        period: this.selectedPeriod,
+        academic_year: this.selectedAcademicYear,
+        teacherID: this.selectedTeacher
       }  
     });
     console.log("subjects", subjects);
@@ -77,14 +84,6 @@ export class AddSubjectsComponent {
       this.newSubjects.clear();
       this.tSubjecForm.updateValueAndValidity();
     }
-  }
-
-  onChangeSubject($event: DropdownChangeEvent): void {
-
-  }
-
-  onChangeAcademicYear($event: DropdownChangeEvent): void {
-
   }
 
 }
